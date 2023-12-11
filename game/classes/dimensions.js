b99dc92f2a5018f2f0d5abad00bfd3d63a906677
Zachary Beyer <zack@boomlearning.com>
2023-12-11 13:14:30 -0700

    shared dimensions

---
+++ game/classes/dimensions.js
@@ -0,0 +1,12 @@
+class Dimensions
+{
+	static buttonPadding = 32;
+	static buttonHeight = Dimensions.buttonPadding * 3;
+	static buttonWidth = Dimensions.buttonPadding * 13;
+	// static game;
+	// static gameHeight = Dimensions.game.config.height;
+	// static gameWidth = Dimensions.game.config.width;
+	static windowWidth = window.innerWidth;
+	static windowHeight = window.innerHeight;
+	static backgroundScale = 1.0;
+}
